87250e96084d351fcb2f3b1caf822982b3bbff58
Luiz Felipe <fc671219@gmail.com>
2021-10-12 20:47:01 -0300

    Fix Database get

--- lib/services/firestore.dart
+++ lib/services/firestore.dart
@@ -1,17 +1,41 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:solotec/models/estacao_model.dart';
 
 class FirestoreManage {
-  FirebaseFirestore _db = FirebaseFirestore.instance;
+  FirestoreManage._();
+  static FirebaseFirestore _db = FirebaseFirestore.instance;
+  static FirebaseAuth _auth = FirebaseAuth.instance;
 
-  CollectionReference<EstacaoModel> getC(
-      String usuarioUiid, String doc, String colection) {
-    return _db
-        .collection(usuarioUiid)
-        .doc(doc)
-        .collection(colection)
+  static Future<void> setEstacao(EstacaoModel model) async {
+    await _db
+        .collection(_auth.currentUser.uid)
+        .doc('Estacoes')
+        .collection('itens')
         .withConverter(
             fromFirestore: (json, _) => EstacaoModel.fromJson(json.data()),
-            toFirestore: (EstacaoModel json, _) => json.toJson());
+            toFirestore: (EstacaoModel json, _) => json.toJson())
+        .doc()
+        .set(model);
+  }
+
+  static Future<List<EstacaoModel>> getEstacao() async {
+    List<EstacaoModel> modelList = <EstacaoModel>[];
+    await _db
+        .collection(_auth.currentUser.uid)
+        .doc('Estacoes')
+        .collection('itens')
+        .withConverter(
+            fromFirestore: (json, _) => EstacaoModel.fromJson(json.data()),
+            toFirestore: (EstacaoModel json, _) => json.toJson())
+        .get()
+        .then((value) {
+      for (QueryDocumentSnapshot item in value.docs) {
+        EstacaoModel m = item.data() as EstacaoModel;
+        modelList.add(m);
+      }
+    });
+
+    return modelList;
   }
 }

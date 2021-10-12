570ed002cea2b0c31d455c0d07654f62bac9101d
Luiz Felipe <fc671219@gmail.com>
2021-10-12 20:43:32 -0300

    Criado

---
+++ lib/models/rede_model.dart
@@ -0,0 +1,17 @@
+class RedeModel {
+  RedeModel(
+      {this.wifiName,
+      this.wifiBSSID,
+      this.wifiIP,
+      this.wifiIPV6,
+      this.wifiSubmask,
+      this.wifiBroadcast,
+      this.wifiGateway});
+  String wifiName;
+  String wifiBSSID;
+  String wifiIP;
+  String wifiIPV6;
+  String wifiSubmask;
+  String wifiBroadcast;
+  String wifiGateway;
+}

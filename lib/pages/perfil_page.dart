a377c529203e59259a6defd5e28d3651a06d6b37
Luiz Felipe <fc671219@gmail.com>
2021-10-12 23:23:04 -0300

    Criado pagina de perfil, somente exibição

---
+++ lib/pages/perfil_page.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+import 'package:provider/provider.dart';
+import 'package:solotec/stores/perfil_store.dart';
+
+class PerfilPage extends StatelessWidget {
+  const PerfilPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle(
+          systemNavigationBarColor: Color(0xffffffff),
+          systemNavigationBarDividerColor: Colors.transparent,
+          systemNavigationBarIconBrightness: Brightness.dark,
+          statusBarColor: Color(0xff612B02),
+          statusBarIconBrightness: Brightness.light,
+        ),
+        child: Provider<PerfilStore>(
+          create: (_) => PerfilStore(),
+          child: Consumer<PerfilStore>(
+            builder: (context, store, _) {
+              return Scaffold(
+                backgroundColor: Colors.white,
+                appBar: AppBar(
+                  brightness: Brightness.dark,
+                  backgroundColor: Color(0xff612B02),
+                  title: Text(
+                    "Perfil",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  /* actions: [
+                    IconButton(
+                      icon: Icon(
+                        Icons.edit,
+                        color: Colors.white,
+                      ),
+                      onPressed: () {},
+                    )
+                  ],*/
+                ),
+                body: ListView(
+                  padding: EdgeInsets.all(15),
+                  children: [
+                    Observer(
+                      builder: (_) {
+                        if (store.imagem != null) {
+                          return CircleAvatar(
+                            backgroundImage: NetworkImage(
+                              store.imagem,
+                            ),
+                            radius: 80,
+                          );
+                        } else {
+                          return CircleAvatar(
+                            radius: 80,
+                            child: (store.anonymous
+                                ? Text('U', style: TextStyle(fontSize: 50))
+                                : (store.imagem != null
+                                    ? Container()
+                                    : Text(
+                                        store.nome.characters
+                                            .characterAt(0)
+                                            .toString(),
+                                        style: TextStyle(fontSize: 50)))),
+                          );
+                        }
+                      },
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Divider(),
+                    ListTile(
+                      title: Text(store.nome),
+                      subtitle: Text('Nome'),
+                    ),
+                    ListTile(
+                      title: Text(store.email),
+                      subtitle: Text('E-mail'),
+                    )
+                  ],
+                ),
+              );
+            },
+          ),
+        ));
+  }
+}
---
+++ lib/stores/perfil_store.dart
@@ -0,0 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:mobx/mobx.dart';
+part 'perfil_store.g.dart';
+
+class PerfilStore = _PerfilStoreBase with _$PerfilStore;
+
+abstract class _PerfilStoreBase with Store {
+  _PerfilStoreBase() {
+    this.anonymous = _auth.currentUser.isAnonymous;
+    this.email = _auth.currentUser.email;
+    this.nome = _auth.currentUser.displayName;
+    this.imagem = _auth.currentUser.photoURL;
+  }
+  FirebaseAuth _auth = FirebaseAuth.instance;
+
+  @observable
+  bool anonymous;
+
+  @observable
+  String nome;
+
+  @observable
+  String email;
+
+  @observable
+  String imagem;
+}
